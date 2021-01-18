dbea0d99ecc031c84cf762d2ebef4cf01df68ec5
chunye <chunye@newsinpalm.com>
2021-01-18 19:27:06 +0800

    python 装饰器

---
+++ docker/start_container.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker run -it --name python_full_stack -p 8031:22 -v /Users/wcy/2_myself_learn/python_full_stack:/python_full_stack jdzw2014/python_full_stack:0.0 /bin/bash
---
+++ lib/2_python_base/2_wrapper.py
@@ -0,0 +1,76 @@
+# !/user/bin/python
+# -*- coding:utf-8 -*-
+"""
+date：          2021-01-18
+Description :
+auther : wcy
+"""
+# import modules
+
+__all__ = []
+
+
+# user login info
+LOGIN_INFO1 = {"test1": "test1_pwd",
+               "test2": "test2_pwd",
+               "test3": "test3_pwd",
+               "test4": "test4_pwd"}
+
+LOGIN_INFO2 = {"test1": "test1_pwd1",
+               "test2": "test2_pwd2",
+               "test3": "test3_pwd3",
+               "test4": "test4_pwd4"}
+
+LOGIN_STATUS = False
+
+
+# define function
+def login_wrapper(notification_type: int):
+    if notification_type == 1:
+        login_info = LOGIN_INFO1
+    elif notification_type == 2:
+        login_info = LOGIN_INFO2
+    else:
+        raise ValueError("wrong notification_type: {}".format(notification_type))
+
+    def login_wrapper_func(func):
+        def wrapper(*args, **kwargs):
+            global LOGIN_STATUS
+            if LOGIN_STATUS:
+                return func(*args, **kwargs)
+            else:
+                name = input("input name: ")
+                pwd = input("input pwd: ")
+
+                if login_info[name] == pwd:
+                    LOGIN_STATUS = True
+                    return func(*args, **kwargs)
+                else:
+                    print("wrong pwd or name")
+        return wrapper
+
+    return login_wrapper_func
+
+
+@login_wrapper(notification_type=1)
+def main_page():
+    print("成功打开 main_page")
+    pass
+
+
+@login_wrapper(notification_type=1)
+def page_info():
+    print("成功打开 page_info")
+    pass
+
+
+@login_wrapper(notification_type=1)
+def similar_goods():
+    print("成功打开 similar_goods")
+    pass
+
+
+# main
+if __name__ == "__main__":
+    main_page()
+    page_info()
