38295b21721648ddb3b12f146bd8912e6a8ebb0e
chunye <chunye@newsinpalm.com>
2021-08-24 10:10:24 +0800

    基于yield实现python协程

--- lib/2_python_base/1_yield/用yield实现Python的协程/1_基于yield实现协程.py
+++ lib/2_python_base/1_yield/用yield实现Python的协程/1_基于yield实现协程.py
@@ -6,16 +6,48 @@ Description :
 auther : wcy
 """
 # import modules
-import os
 
 __all__ = []
 
+
 # define class
+class TaskScheduler(object):
+    def __init__(self):
+        self.task_queue = list()
+
+    def push_task(self, t):
+        self.task_queue.append(t)
+
+    def pop_task(self):
+        return self.task_queue.pop(0)
+
+    def run(self):
+        while self.task_queue:
+            t = self.pop_task()
+            try:
+                t_name = next(t)
+                self.push_task(t)
+            except:
+                print("{} 任务执行结束".format(t_name))
 
 
 # define function
+def task_func(n, task_name):
+
+    num = 0
+    while num < n:
+        print("{} : {}".format(task_name, num))
+        yield task_name
+        num += 1
+
+
+def main():
+    task_scheduler = TaskScheduler()
+    task_scheduler.push_task(task_func(n=10, task_name="task1_10"))
+    task_scheduler.push_task(task_func(n=15, task_name="task2_15"))
+    task_scheduler.run()
 
 
 # main
 if __name__ == '__main__':
-    pass
+    main()
