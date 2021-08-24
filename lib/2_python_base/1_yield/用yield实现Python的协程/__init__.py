068808f2b07139afc091d120d02ebc27f46aafb8
chunye <chunye@newsinpalm.com>
2021-08-24 09:32:39 +0800

    用yield实现协程

---
+++ lib/2_python_base/1_yield/用yield实现Python的协程/__init__.py
@@ -0,0 +1,21 @@
+# !/user/bin/python
+# -*- coding:utf-8 -*-
+"""
+date：          2021-08-24
+Description :
+auther : wcy
+"""
+# import modules
+import os
+
+__all__ = []
+
+# define class
+
+
+# define function
+
+
+# main
+if __name__ == '__main__':
+    pass
