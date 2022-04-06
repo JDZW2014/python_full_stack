87b1d24fc9125df547106ca1abca3a3324b6e209
chunye <chunye@newsinpalm.com>
2022-04-06 10:23:32 +0800

    p2

--- src/4_data_structure/p2/4-荷兰国旗问题.py
+++ src/4_data_structure/p2/4-荷兰国旗问题.py
@@ -5,3 +5,36 @@
 问题二： （荷兰国旗问题）
 问题一： 给定一个数组arr， 和一个数num，请把小于num 的数放在数据组的左边，等于的放中间，大于num的数放在数组的右边。要求额外空间复杂度O(1), 时间复杂度O(N)
 """
+def func1(lis, num):
+    left_boundary = -1
+    for i in range(len(lis)):
+        if lis[i] <= num:
+            left_boundary += 1
+            lis[i], lis[left_boundary] = lis[left_boundary], lis[i]
+    return lis
+
+lis = [1, 3, 2, 2, 5, 4, -1, 9]
+print(func1(lis=lis, num=2))
+
+
+def func2(lis, num):
+    left_boundary = -1
+    right_boundary = len(lis)
+    i = 0
+    while True:
+        if i >= right_boundary:
+            break
+        if lis[i] < num:
+            left_boundary += 1
+            lis[i], lis[left_boundary] = lis[left_boundary], lis[i]
+            i += 1
+        elif lis[i] > num:
+            right_boundary -= 1
+            lis[i], lis[right_boundary] = lis[right_boundary], lis[i]
+        else:
+            i += 1
+    return lis
+
+print(func2(lis=lis, num=2))
+
+    
--- src/4_data_structure/p2/5-快排.py
+++ src/4_data_structure/p2/5-快排.py
@@ -1,22 +1,61 @@
 """
 快排1.0
 """
+import random
+lis = [1, 3, 2, 2, 5, 4, -1, 9]
+def func1(lis, num):
 
-
+    pass
 
 
 
 """
 快排2.0
 """
-
+def func2(lis, num):
+    pass
 
 
 
 """
 快排3.0
 """
+def func3_hl_flag(lis, start, end):
+    # 终止递归条件
+    if (end - start) <= 1:
+        return 
 
+    # 荷兰国旗
+    num = lis[start]
+    left_boundary = start - 1
+    right_boundary = end
+    i = start
+    while True:
+        if i >= right_boundary:
+            break
+        if lis[i] < num:
+            left_boundary += 1
+            lis[i], lis[left_boundary] = lis[left_boundary], lis[i]
+            i += 1
+        elif lis[i] > num:
+            right_boundary -= 1
+            lis[i], lis[right_boundary] = lis[right_boundary], lis[i]
+        else:
+            i += 1
+    
+    # 递归调用
+    # print(num, lis)
+    func3_hl_flag(lis, start, left_boundary+1)
+    func3_hl_flag(lis, right_boundary, end)
+
+def func3(lis):
+    random.shuffle(lis)
+    func3_hl_flag(lis, start=0, end=len(lis))
+    return lis
+
+print(func3(lis))
+
+        
 
 
 
