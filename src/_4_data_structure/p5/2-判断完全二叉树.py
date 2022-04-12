dc957f152dc3104b1b9d9b43a84e529779aa637e
chunye <chunye@newsinpalm.com>
2022-04-12 18:05:56 +0800

    二叉树：40%

---
+++ src/_4_data_structure/p5/2-判断完全二叉树.py
@@ -0,0 +1,21 @@
+# !/user/bin/python
+# -*- coding:utf-8 -*-
+"""
+date：          2022-04-12
+Description :
+auther : wcy
+"""
+# import modules
+import os
+
+__all__ = []
+
+# define class
+
+
+# define function
+
+
+# main
+if __name__ == '__main__':
+    pass
